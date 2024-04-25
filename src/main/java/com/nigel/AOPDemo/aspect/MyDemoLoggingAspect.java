1b1ae854180919b96e9d990350f380e4ae8bdd50
Nigel <mail@nigelmoes.nl>
2024-04-25 11:06:02 -0500

    Added pointcut declarations

--- src/main/java/com/nigel/AOPDemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/nigel/AOPDemo/aspect/MyDemoLoggingAspect.java
@@ -2,6 +2,7 @@ package com.nigel.AOPDemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -16,9 +17,18 @@ public class MyDemoLoggingAspect {
     // @Before("execution(public void add*())")
     // @Before("execution(* add*(com.nigel.AOPDemo.Account, ..))")
     // @Before("execution(* add*(..))")
-    @Before("execution(* com.nigel.AOPDemo.dao.*.*(..))")
+    // @Before("execution(* com.nigel.AOPDemo.dao.*.*(..))")
+
+    @Pointcut("execution(* com.nigel.AOPDemo.dao.*.*(..))")
+    private void forDaoPackage() {}
+
+    @Before("forDaoPackage()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n=====>>> Executing @Before advice on method");
     }
 
+    @Before("forDaoPackage()")
+    public void performApiAnalytics() {
+        System.out.println("\n=====>>> Performing API analytics");
+    }
 }
